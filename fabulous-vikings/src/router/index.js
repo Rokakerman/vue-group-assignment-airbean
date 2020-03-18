d0c78acbbf37027fb1a86917e83908a29b995c42
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-03-18 21:43:02 +0100

    Added View files & structured router

--- fabulous-vikings/src/router/index.js
+++ fabulous-vikings/src/router/index.js
@@ -1,28 +1,32 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import MainView from '../views/MainView.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/about',
+    name: 'About',
+    component: MainView,
+    props: { view: true }
+  },
+  {
+    path: '/menu',
+    name: 'Menu',
+    component: MainView,
+    props: { view: false }
   }
-];
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
-export default router;
+export default router
---
+++ fabulous-vikings/src/views/MainView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="view"><p>About</p></div>
+  <div v-else><p>Menu</p></div>
+</template>
+
+<script>
+export default {
+  props: {
+    view: Boolean
+  }
+}
+</script>
+
+<style></style>
