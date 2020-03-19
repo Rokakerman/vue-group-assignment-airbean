48558d59f6044c4136226ef06d6a23da761b34fc
Jensenbg-Henrik-Johansson <hj@users.noreply.github.com>
2020-03-19 14:13:32 +0100

    MainView uppdaterad
    La till en mer uppdaterad main view. Det som saknar är carticon med en v-if

--- fabulous-vikings/src/views/MainView.vue
+++ fabulous-vikings/src/views/MainView.vue
@@ -1,14 +1,87 @@
 <template>
-  <div v-if="view"><p>About</p></div>
-  <div v-else><p>Menu</p></div>
+  <div class="main-view">
+    <header class="main-header">
+      <div class="header-icon-wrapper"> <figure class="navicon"> <img src="/assets/graphics/navicon.svg"> </figure> </div>
+      <div class="header-icon-wrapper"> </div>
+    </header>
+    <main class="main-content">
+      <div v-if="view"><p>About</p></div>
+      <div v-else><p>Menu</p></div>
+    </main>
+    <footer class="main-footer">
+
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     view: Boolean
-  }
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
 }
 </script>
 
-<style></style>
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
+.main-view {
+  margin: 0px;
+  padding: 0px;
+  height: 100vh;
+  background-color:  #F3E4E1;
+}
+
+.main-header {
+  background: url('/assets/graphics/graphics-header.svg');
+  background-repeat: no-repeat;
+  background-color: #F3E4E1;
+  border: solid black 1px;
+  height: 20%;
+  display: flex;
+}
+
+.header-icon-wrapper {
+  height: 100%;
+  width: 50%;
+  background: none;
+}
+
+.navicon {
+  height: 45px;
+  width: 45px;
+  padding: 0px;
+  margin: 0px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0px 0px 10px;
+}
+
+.main-content {
+  background: url('/assets/graphics/graphics-footer.svg');
+  background-position: bottom;
+  background-repeat: no-repeat;
+  border: solid red 1px;
+  height: 79%;
+}
+
+.main-footer {
+  background: url('/assets/graphics/graphics-footer.svg');
+  background-repeat: no-repeat;
+}
+</style>
+/* eslint-disable */
