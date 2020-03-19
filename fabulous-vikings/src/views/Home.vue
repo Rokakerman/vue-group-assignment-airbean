d98ff5de0882a756e1a641622ca2e55a06c25469
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-03-19 13:54:44 +0100

    added landing page

--- fabulous-vikings/src/views/Home.vue
+++ fabulous-vikings/src/views/Home.vue
@@ -1,26 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home" @click="goToMenu">
+    <div class="inner-cont left-cont"></div>
+    <div class="inner-cont right-cont"></div>
+    <div class="inner-cont central"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
+    goToMenu() {
+      this.$router.push('/menu')
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .home {
+  cursor: pointer;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0e927d;
+  position: relative;
+}
+
+.left-cont {
+  background: url('/assets/graphics/intro-graphic-left.svg');
+  background-size: contain;
+}
+.right-cont {
+  background: url('/assets/graphics/intro-graphic-left.svg');
+  background-size: contain;
+  transform: rotateY(180deg) rotateX(180deg);
+}
+.central {
   background: url('/assets/graphics/airbean-landing.svg');
-  background-color: black;
+  background-size: auto;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.inner-cont {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
   background-repeat: no-repeat;
 }
 </style>
