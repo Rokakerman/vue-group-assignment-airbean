5057083755933cd8ebc6aeea5583c43185147ccf
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-03-20 15:27:07 +0100

    added menu & cart in local storage

---
+++ fabulous-vikings/src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <MenuItem v-for="(el, index) in menu" :key="el.id" :index="index" :item="el" />
+  </section>
+</template>
+
+<script>
+import MenuItem from './MenuItem.vue'
+export default {
+  components: {
+    MenuItem
+  },
+  computed: {
+    menu() {
+      return this.$store.state.menu
+    }
+  },
+  created() {
+    this.$store.dispatch('loadMenu')
+  }
+}
+</script>
+
+<style></style>
